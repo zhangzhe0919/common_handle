bc8743fe4f8f0216c7fbdce09a6c2a125e309604
Escape <escape@EscapedeMacBook-Pro.local>
2020-04-09 13:46:33 +0800

    add date format

--- utils.js
+++ utils.js
@@ -249,4 +249,34 @@ var getUrlParam = function (){
                 rst.sex = sexno % 2 == 0 ? 'F' : 'M';
             }
             return rst;
-        }
+				}
+				
+
+
+// 格式化日期
+function formatDate (date, fmt) { 
+  if (!date || !fmt) {
+    return ''
+  }
+
+  let _that = new Date(date)
+
+  var o = { 
+     "M+" : _that.getMonth()+1,                 //月份 
+     "d+" : _that.getDate(),                    //日 
+     "h+" : _that.getHours(),                   //小时 
+     "m+" : _that.getMinutes(),                 //分 
+     "s+" : _that.getSeconds(),                 //秒 
+     "q+" : Math.floor((_that.getMonth()+3)/3), //季度 
+     "S"  : _that.getMilliseconds()             //毫秒 
+ }; 
+ if(/(y+)/.test(fmt)) {
+         fmt=fmt.replace(RegExp.$1, (_that.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+ }
+  for(var k in o) {
+     if(new RegExp("("+ k +")").test(fmt)){
+          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+      }
+  }
+ return fmt; 
+}      
