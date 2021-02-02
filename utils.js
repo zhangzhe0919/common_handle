c36e099420883a4340416c2762aa408213331050
zhangzhe0313 <37325733+zhangzhe0313@users.noreply.github.com>
2021-02-02 14:12:41 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -362,3 +362,30 @@ function getQueryString (name) {
   r = null;
   return context == null || context === "" || context === "undefined" ? "" : context;
 }
+
+
+function debounce(func, wait, immediate) {
+
+    var timeout, result;
+
+    return function () {
+        var context = this;
+        var args = arguments;
+
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null;
+            }, wait)
+            if (callNow) result = func.apply(context, args)
+        }
+        else {
+            timeout = setTimeout(function(){
+                func.apply(context, args)
+            }, wait);
+        }
+        return result;
+    }
+}
